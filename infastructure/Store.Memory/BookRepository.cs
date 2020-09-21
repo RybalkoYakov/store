334d5ed1a3778046ec7f01dde4214731e3550bb0
JustBadAss <RybalkoYakovv@gmail.com>
2020-09-21 22:26:29 +0300

    Добавить реализацию репозитория книг в памяти

---
+++ infastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C programming languague")
+        };
+
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
